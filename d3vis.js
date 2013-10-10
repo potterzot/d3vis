a75efd3ce4df314233a9de72a8e70943029111dd
potterzot <nick@potterzot.com>
2013-10-10 11:25:54 -0700

    first bar chart

---
+++ d3vis.js
@@ -0,0 +1,15 @@
+/*
+* vis.js - yet another D3JS visualization library, designed specifically for the Measure 2 Manage project.
+* MIT LICENSE
+* Written by Nicholas Potter (potter.nicholas@gmail.com)
+* Source: http://www.github.com/potterzot/vis
+*/
+
+(function(){
+	this.version = "";
+
+})
+
+
+
+
