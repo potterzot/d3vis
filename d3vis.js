602ec2045902d02d90f661fe9a43b70e437bcb2a
potterzot <nick@potterzot.com>
2013-10-11 18:46:03 -0700

    added second y axis

--- d3vis.js
+++ d3vis.js
@@ -14,6 +14,23 @@
 	
 	window.vis = vis;
 
+	vis.yScale = function(graph, d) {
+		var self = graph;
+		var s = self.settings;
+		if (s.yAxis1.enabled) {
+			if (s.xOnY1.indexOf(d.xgroup)!==-1 || (s.xOnY1===false && s.yVars1.indexOf(d.name)!==-1)) {
+				var scale = self[s.yAxis1.scale];
+			}
+			else {
+				var scale = self[s.yAxis0.scale];
+			}
+		}
+		else {
+			var scale = self[s.yAxis0.scale];
+		}
+		return scale(d.value);
+	}
+
 	vis.bar = function(container, settings) {
 		// bar graph - 
 		// container (required) is an svg element (e.g. g or svg) 
@@ -26,6 +43,7 @@
 
 		var defaultSettings = {
 			tooltips: false
+			, palette5a: ["#D7EB3C", "#0D6854", "#E9F681", "#C3CDC3", "#B9F7BC"]
 			, vertical: true
 			, margin: {top: 100, left: 100, right: 100, bottom: 100}
 			, width: 900
@@ -33,30 +51,77 @@
 			, barGap: .1
 			, barStyle: false
 			, xVar: 'x'
-			, yVars: ['y']
+			, yVars0: ['y']
+			, yVars1: false // by default no second y axis
+			, xOnY0: false // by default all x categories are on y axis 0
+			, xOnY1: false // by default no x categories are on y axis 1.
 			, axisMax: 0
 			, axisMin: 0
-			, xAxisPadding: 5 
-			, xAxisOrient: 'bottom'
-			, xAxisTitle: false
-			, xAxisTitleAnchor: 'middle'
-			, xAxisStyle: "font-weight: bold; font-size: 14;"
-			, xAxisTitleStyle: "font-weight: bold; font-size: 14;"
-			, yAxisPadding: 3
-			, yAxisOrient: 'left'
-			, yAxisFormat: '1.3f'
-			, yAxisTitleAnchor: 'middle'
-			, yAxisTitlePadding: 70
-			, yAxisTitleStyle: "font-weight: bold; font-size: 14;"
-			, yAxisTickNumber: 5
-			, titlePadding: 30
-			, titleAlign: 'middle'
-			, titleSize: 18
-			, titleWeight: 'bold'
-			, subTitlePadding: 20
-			, colors: ["#D7EB3C", "#0D6854", "#E9F681", "#C3CDC3", "#B9F7BC"]
+			, xAxis: {
+				enabled: true
+				, padding: 5 
+				, orient: 'bottom'
+				, scale: 'xScale0'
+				, labelSize: 14
+				, labelWeight: 'bold'
+				, title: false
+				, titleAnchor: 'middle'
+				, titleSize: 14
+				, titleWeight: 'bold' 
+			}
+			, yAxis0: {
+				enabled: true
+				, min: 0
+				, padding: 3
+				, orient: 'left'
+				, tickFormat: '1.3f'
+				, ticks: 5
+				, scale: 'yScale0'
+				, labelSize: 14
+				, labelWeight: 'bold'
+				, title: false
+				, titleAnchor: 'middle'
+				, titlePadding: 70
+				, titleSize: 14
+				, titleWeight: 'bold'
+				, titleRotate: -90 
+			}
+			, yAxis1: {
+				enabled: false
+				, min: 0
+				, padding: 3
+				, orient: 'right'
+				, tickFormat: '1.3f'
+				, ticks: 5
+				, scale: 'yScale1'
+				, labelSize: 14
+				, labelWeight: 'bold'
+				, title: false
+				, titleAnchor: 'middle'
+				, titlePadding: -70
+				, titleSize: 14
+				, titleWeight: 'bold' 
+				, titleRotate: -90 
+			}
+			, title: {
+				text: false
+				, padding: 30
+				, align: 'middle'
+				, size: 18
+				, weight: 'bold'
+				, stroke: '#000'
+			}
+			, subTitle: {
+				text: false
+				, padding: 20
+				, align: 'middle'
+				, size: 14
+				, weight: 'normal'
+				, stroke: '#000'
+			}
 			, legend: {
-				stacked: true
+				enabled: true
+				, stacked: true
 				, boxSize: 18
 				, textX: -30
 				, textAnchor: 'end'
@@ -66,32 +131,42 @@
 
 
 		// set bar settings to the user supplied, replacing any missing with the defaults
-		bar.settings = settings || defaultSettings;
+		var s = settings || defaultSettings;
 	 	Object.keys(defaultSettings).forEach(function(key) {
-	 		if (bar.settings[key]===undefined) bar.settings[key] = defaultSettings[key];
+	 		if (s[key]===undefined) s[key] = defaultSettings[key];
 	 	});
 
-	 	['legend', 'margin'].forEach(function(setting) {
-	 		if (bar.settings[setting]) {
+	 	// for settings with subsettings, loop through each subsetting
+	 	['legend', 'margin', 'xAxis', 'yAxis0', 'yAxis1', 'title', 'subTitle'].forEach(function(setting) {
+	 		if (s[setting]) {
 		 		Object.keys(defaultSettings[setting]).forEach(function(key) {
-	 				if (bar.settings[setting][key]===undefined) bar.settings[setting][key] = defaultSettings[setting][key];
+	 				if (s[setting][key]===undefined) s[setting][key] = defaultSettings[setting][key];
 		 		});
 	 		}
 	 	});
 
 	 	// set some settings that depend on others
-	 	bar.settings.plotWidth = bar.settings.plotWidth || bar.settings.width - bar.settings.margin.left - bar.settings.margin.right;
-	 	bar.settings.plotHeight = bar.settings.plotHeight || bar.settings.height - bar.settings.margin.top - bar.settings.margin.bottom;
-	 	bar.settings.xAxisX = bar.settings.xAxisX || bar.settings.width/2;
-	 	bar.settings.xAxisY = bar.settings.xAxisY || bar.settings.height - bar.settings.margin.bottom;
-	 	bar.settings.yAxisX = bar.settings.yAxisX || -bar.settings.plotHeight/2;
-	 	bar.settings.yAxisY = bar.settings.yAxisY || -bar.settings.yAxisTitlePadding;
-	 	bar.settings.titleX = bar.settings.titleX || bar.settings.width/2;
-	 	bar.settings.subtitleX = bar.settings.subtitleX || bar.settings.width/2;
+	 	s.plotWidth = s.plotWidth || s.width - s.margin.left - s.margin.right;
+	 	s.plotHeight = s.plotHeight || s.height - s.margin.top - s.margin.bottom;
+	 	s.xAxis.X = s.xAxis.X || s.width/2;
+	 	s.xAxis.Y = s.xAxis.Y || s.height - s.margin.bottom;
+	 	s.yAxis0.X = s.yAxis0.X || -s.plotHeight/2;
+	 	s.yAxis0.Y = s.yAxis0.Y || -s.yAxis0.titlePadding;
+	 	s.yAxis1.X = s.yAxis1.X || -s.plotHeight/2;
+	 	s.yAxis1.Y = s.yAxis1.Y || -s.yAxis1.titlePadding;
+	 	s.title.X = s.title.X || s.width/2;
+	 	s.subTitle.X = s.subTitle.X || s.width/2;
+
+	 	// colors to use
+	 	(!s.colors) 
+	 		? s.colors = s.palette5a // default color used
+	 		: (typeof(s.colors)==="string") 
+	 			? s.colors = s[s.colors] // user specified a preset color palette
+	 			: s.colors = s.colors; // user specified an array of color values to be used
 
 	 	// legend settings
-	 	var s = bar.settings
-	 	if(s.legend) {
+
+	 	if(s.legend.enabled) {
 			s.legend.textY = s.legend.textY || s.legend.boxSize/2;
 			s.legend.entryTransformY = s.legend.entryTransformY || s.legend.boxSize + 2;
 			s.legend.entryTransform = s.legend.entryTransform || function(d, i) { 
@@ -103,10 +178,40 @@
 	 			return "translate("+ transformX +"," + transformY + ")"; 
 	 		};
 	 	}
-	 	// title settings
-	 	if(s.title) {
-	 		s.titleStyle = s.titleStyle || "font-size:"+s.titleSize+"; font-weight:"+s.titleWeight+";";
-	 	}
+
+	 	// title style if not set to false
+	 	["title", "subTitle"].forEach(function(t) {
+	 		if (s[t].style!==false) {
+	 			s[t].style = s[t].style 
+	 				|| "font-size:"+s[t].size
+	 					+"; font-weight:"+s[t].weight
+	 					+"; font-stroke:"+s[t].stroke
+	 					+";";
+	 		}
+	 	});
+
+	 	// axis settings
+	 	s.xAxis.transform = s.xAxis.transform || "translate(" + 0 + "," + (s.plotHeight + s.margin.top) + ")";
+	 	s.yAxis0.transform = s.yAxis0.transform || "translate("+ s.margin.left + "," + s.margin.top + ")";
+	 	s.yAxis1.transform = s.yAxis1.transform || "translate("+ (s.margin.left + s.plotWidth) + "," + s.margin.top + ")";
+
+	 	["xAxis", "yAxis0", "yAxis1"].forEach(function(axis) {
+	 		// set styles
+	 		if (s[axis].labelStyle!==false) {
+	 			s[axis].labelStyle = s[axis].labelStyle	|| "font-size:"+s[axis].labelSize+";";
+	 			if (s[axis].labelFill) s[axis].labelStyle += " fill:"+s[axis].labelFill+";"; 
+	 			if (s[axis].labelStroke) s[axis].labelStyle += " stroke:"+s[axis].labelStroke+";"; 
+	 			if (s[axis].labelWeight) s[axis].labelStyle += " font-weight:"+s[axis].labelWeight+";"; 
+	 		}
+	 		if (s[axis].titleStyle!==false) {
+	 			s[axis].titleStyle = s[axis].titleStyle || "font-size:"+s[axis].titleSize+";";
+	 			if (s[axis].titleFill) s[axis].titleStyle += "fill:"+s[axis].titleFill+";";
+	 			if (s[axis].titleStroke) s[axis].titleStyle += " stroke:"+s[axis].titleStroke+";";
+	 			if (s[axis].titleWeight) s[axis].titleStyle += " font-weight:"+s[axis].titleWeight+";";
+	 		} 
+	 	});
+
+	 	bar.settings = s;
 
 		bar.build = function() {
 			var self = this;
@@ -123,38 +228,39 @@
 				.rangeRoundBands([s.margin.left,s.plotWidth+s.margin.left], s.barGap);
 
 			self.xScale1 = d3.scale.ordinal(); // scale within a given x category
+		
+			// scale for yAxis 0	
+			self.yScale0 = d3.scale.linear()
+				.range([s.plotHeight,s.xAxis.padding]);
 			
-			self.yScale = d3.scale.linear()
-				.range([s.plotHeight,s.xAxisPadding]);
+			// create the scale for yAxis 1	
+			if(s.yAxis1.enabled) self.yScale1 = d3.scale.linear().range([s.plotHeight,s.xAxis.padding]);
 
 			self.colorScale = d3.scale.ordinal()
 				.range(s.colors);
 
 			// create axes
-			self.xAxis = d3.svg.axis()
-				.scale(self.xScale0)
-				.orient(s.xAxisOrient);
-			self.yAxis = d3.svg.axis()
-				.scale(self.yScale)
-				.orient(s.yAxisOrient)
-				.ticks(s.yAxisTickNumber);
-
-			// if a format is indicated, then set it, otherwise don't
-			if(s.xAxisFormat) self.xAxis.tickFormat(d3.format(s.xAxisFormat));
-			if(s.yAxisFormat) self.yAxis.tickFormat(d3.format(s.yAxisFormat));
+			["xAxis", "yAxis0", "yAxis1"].forEach(function(axis) {
+				self[axis] = d3.svg.axis();
+				if (s[axis].scale) self[axis].scale(self[s[axis].scale]);
+				if (s[axis].orient) self[axis].orient(s[axis].orient);	
+				if (s[axis].tickFormat) self[axis].tickFormat(d3.format(s[axis].tickFormat));	
+				if (s[axis].ticks)self[axis].ticks(s[axis].ticks);	
+				if (s[axis].tickValues)self[axis].tickValues(s[axis].tickValues);	
+			});
 
 			// create the main title
-			if(s.title) {
-				s.title.forEach(function(title, i) {
+			if(s.title.text) {
+				s.title.text.forEach(function(title, i) {
 					var t = self.g.append('g')
 						.attr('class', 'vis-title')
 						.append('text')
-							.attr('x', s.titleX)
-							.attr('y', s.titlePadding + (i)*s.titleSize)
-							.attr('text-anchor', s.titleAlign);
+							.attr('x', s.title.X)
+							.attr('y', s.title.padding + (i)*(s.title.size+2))
+							.attr('text-anchor', s.title.align);
 
 					// create the style if needed
-					if(s.titleStyle) t.attr('style', s.titleStyle);
+					if(s.title.style) t.attr('style', s.title.style);
 					// the text
 					t.text(title);
 				})	
@@ -162,53 +268,45 @@
 			}
 
 			// create the subtitle
-			if(s.subtitle) {
+			if(s.subTitle.text) {
 				var subtitle = self.g.append('g')
 					.attr('class', 'vis-subtitle')
 					.append('text')
-						.attr('x', s.subtitleX)
-						.attr('y', s.titlePadding + s.subTitlePadding)
-						.attr('text-anchor', s.titleAlign);
+						.attr('x', s.subTitle.X)
+						.attr('y', s.title.padding + s.subTitle.padding)
+						.attr('text-anchor', s.subTitle.align);
 
-				if(s.subTitleStyle) subtitle.attr('style', s.subTitleStyle);
-				subtitle.text(s.subtitle);
+				if(s.subTitleStyle) subtitle.attr('style', s.subTitle.style);
+				subtitle.text(s.subtitle.text);
 			}
 
 			// draw the axes
 		    // draw the x axis
-			var xaxis = self.g.append("g")
-		    	.attr("class", "x axis")
-		      	.attr("transform", "translate(" + 0 + "," + (s.plotHeight + s.margin.top) + ")")
-		      	.call(self.xAxis);
+		    ['xAxis', 'yAxis0', 'yAxis1'].forEach(function(axis) {
+		    	if(s[axis].enabled===true) {
+		    		// axis
+		    		var thisAxis = self.g.append('g')
+		    			.attr("class", "axis "+axis)
+		      			.attr("transform", s[axis].transform)
+		      			.call(self[axis]);
 
-		    var xaxisTitle = xaxis.append('text')
-		      		.attr('class', 'x axis text')
-		      		.attr('y', s.xAxisY)
-		      		.attr('x', s.xAxisX)
-		      		.attr('dy', '.71em')
-		      		.attr('text-anchor', s.xAxisTitleAnchor)
-		      		.text(s.xAxisTitle);
+					if(s[axis].labelStyle) thisAxis.attr('style', s[axis].labelStyle);
 
-			if(s.xAxisStyle) xaxis.attr('style', s.xAxisStyle);
-			if(s.xAxisTitleStyle) xaxisTitle.attr('style', s.xAxisTitleStyle);
-
-		    // draw the y axis
-			var yaxis = self.g.append("g")
-		    	.attr("class", "y axis")
-		    	.attr("transform", "translate("+ s.margin.left + "," + s.margin.top + ")")
-		      	.call(self.yAxis);
-
-		    var yaxisTitle = yaxis.append("text")
-			    	.attr('class', 'y axis text')
-		    		.attr("transform", "rotate(-90)")
-		      		.attr("y", s.yAxisY)
-		      		.attr("x", s.yAxisX)
-		      		.attr("dy", ".71em")
-		      		.attr('text-anchor', s.yAxisTitleAnchor)
-		      		.text(s.yAxisTitle);
-
-			if(s.yAxisStyle) yaxis.attr('style', s.yAxisStyle);
-			if(s.yAxisTitleStyle) yaxisTitle.attr('style', s.yAxisTitleStyle);
+					// axis title
+		      		if(s[axis].title) {
+		      			var axisTitle = thisAxis.append('text')
+			      			.attr('class', 'axis text '+axis)
+				      		.attr('y', s[axis].Y)
+				      		.attr('x', s[axis].X)
+				      		.attr('dy', '.71em')
+				      		.attr('text-anchor', s[axis].titleAnchor)
+				      		.text(s[axis].title);
+						
+						if(s[axis].titleRotate) axisTitle.attr('transform', "rotate("+s[axis].titleRotate+")");	
+						if(s[axis].titleStyle) axisTitle.attr('style', s[axis].titleStyle);
+				      }
+		    	}
+		    });
 
 			// build the tips if set
 			if(s.tooltips) {
@@ -232,7 +330,7 @@
 				if(s.legend.style) legend.attr('style', s.legend.style);
 
 				var legendEntry = legend.selectAll(".legend-entry")
-      				.data(s.yVars.slice())
+      				.data(s.yVars0.slice())
     				.enter().append("g")
       				.attr("class", "legend-entry")
       				.attr("transform", s.legend.entryTransform);
@@ -267,34 +365,57 @@
 
 			// reorder the data for graphing
 			data.forEach(function(d) {
-				d.y = s.yVars.map(function(yVar) { return {name: yVar, value: +d[yVar]}; })
+				d.y = s.yVars0.map(function(yVar) { return {xgroup: d[s.xVar], name: yVar, value: +d[yVar]}; })
 			})
 			self.data = data;
 
 			// adjust the scale and redraw the axis
 			// maximum and minimum
-			var max = s.yAxisMax || d3.max([d3.max(data.map(function(d) { return d[s.yVars[0]]; })), s.axisMax]);
-			var min = s.yAxisMin || d3.min([d3.min(data.map(function(d) { return d[s.yVars[0]]; })), s.axisMin]);
-
+			var max0 = (s.yAxis0.max!==false)
+				? s.yAxis0.max
+				: d3.max(data.map(function(d) { return +d[s.yVars0[0]]; }));
+			
+			var min0 = (s.yAxis0.min!==false) 
+				? s.yAxis0.min
+				: d3.min(data.map(function(d) { return +d[s.yVars0[0]]; }));
+			
+			var max1 = (s.yAxis1.max!==false)
+				? s.yAxis1.max
+				: d3.max(data.map(function(d) { return +d[s.yVars1[0]]; }));
+			
+			var min1 = (s.yAxis1.min!==false) 
+				? s.yAxis1.min
+				: d3.min(data.map(function(d) { return +d[s.yVars1[0]]; }));
+			
 			// x axis
-			self.xScale0.domain(data.map(function(d) { return d[s.xVar]; }));
-			self.xAxis.scale(self.xScale0);
- 			self.g.select('.x.axis')
+			self[s.xAxis.scale].domain(data.map(function(d) { return d[s.xVar]; }));
+			self.xAxis.scale(self[s.xAxis.scale]);
+ 			self.g.select('.xAxis.axis')
  				.transition()
  				.duration(500)
  				.call(self.xAxis);
 		
 			// within each x category
-			self.xScale1.domain(s.yVars).rangeRoundBands([0, self.xScale0.rangeBand()]);
+			self.xScale1.domain(s.yVars0).rangeRoundBands([0, self.xScale0.rangeBand()]);
 
-			// y axis
-			self.yScale.domain([0,max]);
-			self.yAxis.scale(self.yScale);
-    		self.g.select('.y.axis')
+			// y axis 0
+			self[s.yAxis0.scale].domain([min0,max0]);
+			self.yAxis0.scale(self[s.yAxis0.scale]);
+    		self.g.select('.yAxis0.axis')
     			.transition()
     			.duration(500)
-    			.call(self.yAxis);
+    			.call(self.yAxis0);
 
+    		// y axis 1
+    		if (s.yAxis1.enabled) {
+				self[s.yAxis1.scale].domain([min1,max1]);
+				self.yAxis1.scale(self[s.yAxis1.scale]);
+	    		self.g.select('.yAxis1.axis')
+	    			.transition()
+	    			.duration(500)
+	    			.call(self.yAxis1);
+
+    		}
 	    	// select the bar groups (each category)
 		  	var barGroups = self.g.selectAll(".bargroup")
 		  		.data(data)
@@ -310,15 +431,15 @@
 	  			.attr('class', function(d) { return 'bar '+d.name; })
 	  			.attr('x', function(d) { return self.xScale1(d.name); })
 	  			.attr('width', self.xScale1.rangeBand())
-	  			.attr('y', function(d) { return self.yScale(d.value); })
-	  			.attr('height', function(d) { return d3.max([s.plotHeight - s.xAxisPadding - self.yScale(d.value), 0]); })
+	  			//.attr('y', function(d) { return self.yScale0(d.value); })
+	  			//.attr('height', function(d) { return d3.max([s.plotHeight - s.xAxis.padding - self.yScale0(d.value), 0]); })
 	      		.on('mouseover', function(d) { return (s.tooltips) ? self.tip.show : null; })
 	      		.on('mouseout', function(d) { return (s.tooltips) ? self.tip.hide : null; });
 
 	      	var barsUpdate = bars.transition()
 	      		.duration(s.duration)
-		      	.attr("y", function(d) { return self.yScale(d.value) + s.margin.top; })
-	  			.attr('height', function(d) { return d3.max([s.plotHeight - s.xAxisPadding - self.yScale(d.value), 0]); });
+		      	.attr("y", function(d) { return vis.yScale(self, d) + s.margin.top; })
+	  			.attr('height', function(d) { return d3.max([s.plotHeight - s.xAxis.padding - vis.yScale(self, d), 0]); });
 
 	      	var barsExit = bars.exit().transition()
 	      		.duration(s.duration)
@@ -327,6 +448,7 @@
 	      	(s.barStyle) 
 	      		? barsEnter.attr('style', function(d) { return s.barStyle+"fill:"+self.colorScale(d.name)+";"; })
 	      		: barsEnter.attr('style', function(d) { return "fill:"+self.colorScale(d.name)+";"; });
+		
 		}
 
 		bar.container = container;
